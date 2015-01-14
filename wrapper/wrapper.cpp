e4891f51fe4561daf2c5ec722df7d1cbcd1eefdf
Nikolas Havrikov <kolya@stud.uni-saarland.de>
2015-01-14 22:23:19 +0100

    added fork+execv+waitpid for calling the test driver

--- wrapper/wrapper.cpp
+++ wrapper/wrapper.cpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <cstdlib>
 #include <unistd.h>
+#include <sys/wait.h>
 
 std::vector<std::string> &split(const std::string &s, char delim,
 		std::vector<std::string> &elems) {
@@ -24,20 +25,26 @@ std::vector<std::string> split(const std::string &s, char delim) {
 }
 
 void PIN_SCORE_START() {
-
+	std::cout << "PIN_SCORE_START" << std::endl;
 }
 
 void PIN_SCORE_END() {
-
+	std::cout << "PIN_SCORE_END" << std::endl;
 }
 
-int main() {
-
+/* EXPECTED ARGUMENTS:
+ * controlSocket transport address // TODO
+ * dataIn transport address // TODO
+ * path to driver
+ */
+int main(int argc, char* argv[]) {
+	assert(argc > 3);
 	// Prepare our context and socket
 	zmq::context_t context(1);
 	zmq::socket_t controlSocket(context, ZMQ_REQ);
 	zmq::socket_t dataIn(context, ZMQ_PULL);
 
+	// TODO receive transport addresses via parameter
 	dataIn.connect("tcp://127.0.0.1:5556");
 	controlSocket.connect("tcp://127.0.0.1:5555");
 
@@ -46,17 +53,45 @@ int main() {
 	memcpy((void *) readiness.data(), "rdy", 3);
 	controlSocket.send(readiness);
 
+
 	while (true) {
+		std::cout << "Wrapper waiting for tasks" << std::endl;
 		// Wait for data from xmlmate
 		std::string data = s_recv(dataIn);
 		std::vector<std::string> items = split(data, ':');
 //		std::cout << "Received " << items.size() << " work items" << std::endl;
-		PIN_SCORE_START();
-		// process data
-		for (std::vector<std::string>::iterator it = items.begin(); it != items.end(); ++it) {
-			// TODO call driver here
+		char **args = new char*[items.size()+argc-2];
+		int i = 0;
+		for(i = 0; i < argc-3; ++i) {
+			args[i] = argv[i+3];
 		}
-		PIN_SCORE_END();
+		for (std::vector<std::string>::iterator it = items.begin(); it != items.end(); ++it) {
+			args[i] = const_cast<char*>(it->c_str());
+			i += 1;
+		}
+		args[i] = NULL;
+
+		pid_t pid = fork();
+		switch (pid) {
+		case -1:
+			// error forking
+			std::cerr << "ERROR: Could not fork driver process!" << std::endl;
+			delete args;
+			return 1;
+		case 0:
+			// child process
+			PIN_SCORE_START();
+			execv(argv[3], args); // call driver here
+			return 0;
+		default:
+			// parent process
+			int status;
+			waitpid(pid, &status, 0);
+//			std::cout << "driver exited with code " << status << std::endl;
+			delete args;
+			PIN_SCORE_END();
+			break;
+		}
 	}
 
 	// unreachable
