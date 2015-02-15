b9f2de59df42cc4a81829ab453247ef94702c85e
Nikolas Havrikov <kolya@stud.uni-saarland.de>
2015-02-15 21:23:53 +0100

    Added a simple utility to list the names of simple types encountered in the schema.

---
+++ xmlmate/src/xmlmate/java/org/xmlmate/xml/metrics/TypeNameLister.java
@@ -0,0 +1,44 @@
+package org.xmlmate.xml.metrics;
+
+import org.apache.xerces.xs.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xmlmate.XMLProperties;
+
+import java.util.HashSet;
+
+public class TypeNameLister implements SchemaElementVisitor {
+    private static final Logger logger = LoggerFactory.getLogger(TypeNameLister.class);
+    private final HashSet<String> seenTypes;
+
+
+    public TypeNameLister() {
+        seenTypes = new HashSet<>();
+    }
+
+    @Override
+    public void visit(XSElementDeclaration decl) {
+        XSTypeDefinition td = decl.getTypeDefinition();
+        XSSimpleTypeDefinition stype = null;
+        if (td.getTypeCategory() == XSTypeDefinition.SIMPLE_TYPE) {
+            stype = (XSSimpleTypeDefinition) td;
+        } else {
+            XSComplexTypeDefinition ctype = (XSComplexTypeDefinition) td;
+            XSObjectList attrs = ctype.getAttributeUses();
+            for (int i = 0; i < attrs.getLength(); i++) {
+                XSAttributeUse use = (XSAttributeUse) attrs.item(i);
+                String name = use.getAttrDeclaration().getTypeDefinition().getName();
+                if (seenTypes.add(name))
+                    logger.info("Type {}", name);
+            }
+            if (ctype.getContentType() == XSComplexTypeDefinition.CONTENTTYPE_SIMPLE) {
+                stype = ctype.getSimpleType();
+            }
+        }
+        if (stype != null) {
+            String name = stype.getName();
+            if (seenTypes.add(name))
+                logger.info("Type {}", name);
+        }
+    }
+}
