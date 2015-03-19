a5ec1f1bf262f07621208fa8c1d2eeaa8d538f14
Nikolas Havrikov <kolya@stud.uni-saarland.de>
2015-03-19 18:41:05 +0100

    started work on buffer overflow fitness function pintool

---
+++ pintools/bufoverflow/bufoverflow.cpp
@@ -0,0 +1,196 @@
+#include <pin.H>
+#include <zmq.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+#include <set>
+#include <map>
+#include <msgpack.hpp>
+
+typedef std::pair<ADDRINT, ADDRINT> Range;
+
+struct RangeCompare {
+    //overlapping ranges are considered equivalent
+    bool operator()(const Range& lhv, const Range& rhv) const {
+        return lhv.second < rhv.first;
+    }
+};
+typedef std::set<Range, RangeCompare> Allocations_T;
+
+// ^ adapted from http://stackoverflow.com/a/8561876
+
+Range *find_range(const Allocations_T& ranges, ADDRINT value) {
+    Allocations_T::const_iterator it = ranges.find(Range(value, value));
+    if (it == ranges.end())
+    	return NULL;
+    return (Range*)&(*it);
+}
+
+bool in_range(const Allocations_T& ranges, ADDRINT value) {
+	return find_range(ranges, value) != NULL;
+}
+typedef std::map<ADDRINT, ADDRINT> Acessess_T;
+
+/* ================================================================== */
+// Global variables 
+/* ================================================================== */
+
+static string targetImage = "";	//name of the targeted binary image
+static ADDRINT imgHigh = 0;		//low end of target image
+static ADDRINT imgLow = 0;			//high end of binary image
+static Allocations_T allocations; // stores all currently allocated memory ranges
+static Acessess_T accesses; // maps return site of a malloc call to maximum distance from its buffer
+
+/* ===================================================================== */
+// Command line switches
+/* ===================================================================== */
+
+KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "",
+		"specify file name for bblcov output");
+
+KNOB<BOOL> KnobCount(KNOB_MODE_WRITEONCE, "pintool", "count", "1",
+		"count instructions, basic blocks and threads in the application");
+
+KNOB<string> KnobTargetImage(KNOB_MODE_WRITEONCE, "pintool", "target", "",
+		"specify the name of the targeted image");
+
+/* ===================================================================== */
+// Utilities
+/* ===================================================================== */
+
+INT32 Usage() {
+	cerr << "This tool prints out the number of dynamically executed " << endl
+			<< "instructions, basic blocks and threads in the application."
+			<< endl << endl;
+	cerr << KNOB_BASE::StringKnobSummary() << endl;
+	return -1;
+}
+
+
+VOID resetCounters(void *ptr) {
+	free(ptr);
+	allocations.clear();
+}
+
+void *SendResults(uint32_t id, void *socket) {
+	msgpack::sbuffer buffer;
+	msgpack::pack(buffer, id);
+	msgpack::pack(buffer, false);
+	msgpack::pack(buffer, accesses);
+
+	size_t len = buffer.size();
+	void *buf = malloc(len);
+	if (NULL != buf) {
+		memcpy(buf, buffer.data(), len);
+		zmq_send_const(socket, buf, len, 0);
+//		cerr << "Sent " << ret << " bytes." << endl;
+	}
+	return buf;
+}
+
+/* ===================================================================== */
+// Analysis callbacks
+/* ===================================================================== */
+
+VOID Arg1Before(CHAR * name, ADDRINT size, ADDRINT retIP) {
+    // record retIP -> size
+}
+
+VOID MallocAfter(ADDRINT ret) {
+	// how to identify stored size?
+}
+
+VOID RecordMemAccess(VOID *ip, ADDRINT *ea) {
+	// TODO
+}
+
+/* ===================================================================== */
+// Instrumentation callbacks
+/* ===================================================================== */
+
+// Pin calls this function every time a new img is loaded
+// Note that imgs (including shared libraries) are loaded lazily
+VOID ImageLoad(IMG img, VOID *v) {
+	cout << "Loading image " << IMG_Name(img) << endl;
+	if (IMG_Name(img).find(targetImage) != std::string::npos) {
+		cout << "Loaded target image " << IMG_Name(img) << endl;
+		imgLow = IMG_LowAddress(img);
+		imgHigh = IMG_HighAddress(img);
+
+	}
+}
+
+VOID Trace(TRACE trace, VOID *v) {
+	if (0 == imgLow)
+		return;
+	for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl)) {
+		ADDRINT addr = BBL_Address(bbl);
+		if (addr < imgLow || addr > imgHigh)
+			continue;
+		for (INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins)) {
+			if (INS_IsMemoryRead(ins) || INS_IsMemoryWrite(ins)) {
+				UINT32 memoryOperands = INS_MemoryOperandCount(ins);
+				for (UINT32 memOp = 0; memOp < memoryOperands; memOp++) {
+						INS_InsertPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR) RecordMemAccess, IARG_INST_PTR, IARG_MEMORYOP_EA, memOp, IARG_END);
+				}
+			}
+		}
+	}
+}
+
+VOID Routine(RTN rtn, VOID *v) {
+	if (RTN_Name(rtn).find("PIN_SCORE_START") != std::string::npos) {
+		cout << "Detected PIN_SCORE_START" << endl;
+		RTN_Open(rtn);
+		RTN_Replace(rtn, (AFUNPTR) resetCounters);
+		RTN_Close(rtn);
+	} else if (RTN_Name(rtn).find("PIN_SCORE_END") != std::string::npos) {
+		cout << "Detected PIN_SCORE_END" << endl;
+		RTN_Open(rtn);
+		RTN_Replace(rtn, (AFUNPTR) SendResults);
+		RTN_Close(rtn);
+	}
+}
+
+/*!
+ * The main procedure of the tool.
+ * This function is called when the application image is loaded but not yet started.
+ * @param[in]   argc            total number of elements in the argv array
+ * @param[in]   argv            array of command line arguments, 
+ *                              including pin -t <toolname> -- ...
+ */
+int main(int argc, char *argv[]) {
+
+	PIN_InitSymbols();
+
+// Initialize PIN library. Print help message if -h(elp) is specified
+// in the command line or the command line is invalid
+	if (PIN_Init(argc, argv)) {
+		return Usage();
+	}
+
+	targetImage = KnobTargetImage.Value();
+
+	if (targetImage.empty()) {
+		cerr << "Please provide the name of the target image!" << endl;
+		return Usage();
+	}
+
+// Register ImageLoad to be called when an image is loaded
+	IMG_AddInstrumentFunction(ImageLoad, 0);
+
+// Register Routine to be called to instrument trace
+	TRACE_AddInstrumentFunction(Trace, 0);
+
+	RTN_AddInstrumentFunction(Routine, 0);
+
+// Start the program, never returns
+	PIN_StartProgram();
+
+	return 0;
+}
+
+/* ===================================================================== */
+/* eof */
+/* ===================================================================== */
