878b36c848300a85ab1e6d55962eb74e68aedcc5
Nikolas Havrikov <kolya@stud.uni-saarland.de>
2015-07-15 09:21:42 +0200

    fixes for schema and converter

--- subjects/jpeg/converters/xml2jpeg.py
+++ subjects/jpeg/converters/xml2jpeg.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import sys
 import io
 from xml.etree.ElementTree import parse
 import struct
 from array import array
-import os
+from builtins import range, map
+import random
+from bitarray import bitarray
+from itertools import count
 
 ns = {'jfif': 'http://xmlmate.org/schemas/jfif'}
 
 
-def random_data(length):
-    urandom = os.urandom
-    return bytearray(urandom(length)).replace(b'0xFF', b'0xFE')
+def random_data(length, seed):
+    random.seed(seed)
+    return bytearray(random.getrandbits(8) for _ in range(length))
 
 
 def write_jfif(jfif, f):
@@ -22,43 +24,45 @@ def write_jfif(jfif, f):
     segment_length = 16 + thumbnail_size
     # JFIF-APP0
     f.write(struct.pack('>HHBBBBBBBBHHBB',
-                        0xFFE0,                        # APP0
-                        segment_length,                # segment_length
+                        0xFFE0,  # APP0
+                        segment_length,  # segment_length
                         0x4A, 0x46, 0x49, 0x46, 0x00,  # JFIF identifier
-                        int(jfif[0].text),             # version major
-                        int(jfif[1].text),             # version minor
-                        int(jfif[2].text),             # density units
-                        int(jfif[3].text),             # Xdensity
-                        int(jfif[4].text),             # Ydensity
-                        x_thumbnail,                   # Xthumbnail
-                        y_thumbnail                    # Ythumbnail
+                        int(jfif[0].text),  # version major
+                        int(jfif[1].text),  # version minor
+                        int(jfif[2].text),  # density units
+                        int(jfif[3].text),  # Xdensity
+                        int(jfif[4].text),  # Ydensity
+                        x_thumbnail,  # Xthumbnail
+                        y_thumbnail  # Ythumbnail
                         ))
-    f.write(random_data(thumbnail_size))               # thumbnail data
+    seed = int(jfif[7].text)
+    f.write(random_data(thumbnail_size, seed))  # thumbnail data
 
 
 def write_jfxx(jfxx, f):
     x_thumbnail = int(jfxx[0][0].text)
     y_thumbnail = int(jfxx[0][1].text)
+    seed = int(jfxx[0][2].text)
     thumbnail_size = x_thumbnail * y_thumbnail
     palette = jfxx[0].find('./jfif:palette', ns)
     segment_length = 10 + (thumbnail_size * 3 if palette is None else thumbnail_size + 768)
     thumbnail_format = 0x11 if palette is not None else 0x13
     # JFXX-APP0
     f.write(struct.pack('>HHBBBBBBBB',
-                        0xFFE0,                        # APP0
-                        segment_length,                # length
+                        0xFFE0,  # APP0
+                        segment_length,  # length
                         0x4A, 0x46, 0x58, 0x58, 0x00,  # JFXX identifier
-                        thumbnail_format,              # thumbnail format
-                        x_thumbnail,                   # Xthumbnail
-                        y_thumbnail                    # Ythumbnail
+                        thumbnail_format,  # thumbnail format
+                        x_thumbnail,  # Xthumbnail
+                        y_thumbnail  # Ythumbnail
                         ))
     if palette is not None:
         palette_values = palette.iter()
         next(palette_values)  # skip the palette value itself
         f.write(array('B', map(lambda x: int(x.text), palette_values)))
-        f.write(random_data(thumbnail_size))
+        f.write(random_data(thumbnail_size, seed))
     else:
-        f.write(random_data(thumbnail_size * 3))
+        f.write(random_data(thumbnail_size * 3, seed))
 
 
 def write_dqt(dqt, f, num):
@@ -66,8 +70,8 @@ def write_dqt(dqt, f, num):
     next(data)
     f.write(struct.pack('>HHB',
                         0xFFDB,  # FFDB
-                        0x43,    # length = 67
-                        num      # destination in [1,4]
+                        0x43,  # length = 67
+                        num  # destination in [0,3]
                         ))
     f.write(array('B', map(lambda x: int(x.text), data)))  # 64 bytes of data
 
@@ -78,34 +82,46 @@ def write_sof(sof, f):
     assert 1 <= num <= 3
     length = 8 + 3 * num
     f.write(struct.pack('>HHBHHB',
-                        0xFFC0,            # FFC0
-                        length,            # length
-                        8,                 # bit depth
-                        int(sof[0].text),  # width
-                        int(sof[1].text),  # height
-                        num                # num components
+                        0xFFC0,  # FFC0
+                        length,  # length
+                        8,  # bit depth
+                        int(sof[0].text),  # height
+                        int(sof[1].text),  # width
+                        num  # num components
                         ))
     for component in components:
         f.write(struct.pack('>BBB',
                             int(component[0].text),  # identifier
                             int(component[1].text),  # Hi+Vi
-                            int(component[2].text)   # DQT selector
+                            int(component[2].text)  # DQT selector
                             ))
 
 
+def huffman(seed):
+    random.seed(seed)
+    vals = []
+    for i in range(1, 17):
+        ul = 2 ** min(i, 8)
+        ul = min(ul, ul - sum(map(lambda xy: xy[1] * 2 ** (i - xy[0]), enumerate(vals, start=1))))
+        x = random.randint(0, max(0, ul))
+        # print('i=%d \t%d <= %d' % (i, x, ul))
+        vals.append(x)
+    return vals
+
+
 def write_dht(dht, f):
-    bit_items = list(dht)[1:]
-    assert len(bit_items) == 16
-    bits = [min(int(b.text), 2**(i+1)-1) for (i, b) in enumerate(bit_items)]  # constrain BITS[i] < 2^(i+1)
+    bitseed = int(dht[1].text)
+    bits = bytearray(huffman(bitseed))
     nhv = sum(bits)
     length = 19 + nhv
     f.write(struct.pack('>HHB',
-                        0xFFC4,           # FFC4
-                        length,           # length
+                        0xFFC4,  # FFC4
+                        length,  # length
                         int(dht[0].text)  # class + destination
                         ))
     f.write(array('B', bits))
-    f.write(random_data(nhv))  # FIXME probably need to generate unique bytes
+    nhvseed = int(dht[2].text)
+    f.write(random_data(nhv, nhvseed))
 
 
 def write_sos(sos, f):
@@ -116,12 +132,12 @@ def write_sos(sos, f):
     f.write(struct.pack('>HHB',
                         0xFFDA,  # FFDA
                         length,  # length
-                        num      # num components
+                        num  # num components
                         ))
     for component in components:
         f.write(struct.pack('>BB',
                             int(component[0].text),  # identifier
-                            int(component[1].text)   # DC + AC
+                            int(component[1].text)  # DC + AC
                             ))
     f.write(struct.pack('>BBB', 0, 63, 0))
 
@@ -133,8 +149,8 @@ def xml2jpeg(input_file, output_file):
     assert jfif is not None
     sof = root.find('./jfif:sof', ns)
     assert sof is not None
-    width = int(sof[0].text)
-    height = int(sof[1].text)
+    height = int(sof[0].text)
+    width = int(sof[1].text)
     dhts = root.findall('./jfif:dht', ns)
     assert dhts
     sos = root.find('./jfif:sos', ns)
@@ -158,9 +174,30 @@ def xml2jpeg(input_file, output_file):
         for dht in dhts:
             write_dht(dht, f)
         write_sos(sos, f)
-        f.write(random_data(32))
+        # f.write(random_data(1920*1080, 5))  # TODO
         f.write(struct.pack('>H', 0xFFD9))  # EOI
 
 
 if __name__ == '__main__':
-    xml2jpeg(sys.argv[1], sys.argv[2])
+    # while True:
+    #     seed = random.randrange(2**32)
+    #     if not sum(list(huffman(seed))[:4]):
+    #         print(seed)
+    #         break
+
+    # huff = huffman(13)
+    # nhvs = random_data(sum(huff), 37)
+    # print(huff)
+    # print(list(map(str, nhvs)))
+
+    # d = {}
+    # c = count()
+    # for p, n in enumerate(huff, start=1):
+    #     for h in range(n):
+    #         k = nhvs[h]
+    #         v = bitarray(bin(next(c))[2:].zfill(p), endian='big')
+    #         d[k] = v
+    # print(d)
+
+    from sys import argv
+    xml2jpeg(argv[1], argv[2])
